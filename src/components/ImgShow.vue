ac9a737e35359a451d0f62df4dbe8ed9679ff58a
MingMin.Shi <mingmin.shi@marautec.com>
2021-01-06 09:35:04 +0800

    左键标点右键拖动

--- src/components/ImgShow.vue
+++ src/components/ImgShow.vue
@@ -80,24 +80,82 @@ export default {
       var _this = this;
       var canvas = _this.canvas;
 
+      canvas.oncontextmenu = function(e) {
+        e.preventDefault();
+      };
+
       canvas.onmousedown = function(event) {
+        event.stopPropagation();
+        event.preventDefault();
+
         let pos = _this.windowToCanvas(event.clientX, event.clientY);
-        canvas.onmousemove = function(evt) {
-          //移动
-          canvas.style.cursor = "move";
-          var pos1 = _this.windowToCanvas(evt.clientX, evt.clientY);
-          var x = pos1.x - pos.x;
-          var y = pos1.y - pos.y;
-          pos = pos1;
-          _this.imgX += x;
-          _this.imgY += y;
-          _this.drawImage(); //重新绘制图片
-        };
-        canvas.onmouseup = function() {
-          canvas.onmousemove = null;
-          canvas.onmouseup = null;
-          canvas.style.cursor = "default";
-        };
+
+        if (event.button == 2) {
+          //鼠标右击事件
+          canvas.onmousemove = function(evt) {
+            //移动
+            canvas.style.cursor = "move";
+            var pos1 = _this.windowToCanvas(evt.clientX, evt.clientY);
+            var x = pos1.x - pos.x;
+            var y = pos1.y - pos.y;
+            pos = pos1;
+            _this.imgX += x;
+            _this.imgY += y;
+            _this.drawImage(); //重新绘制图片
+          };
+          canvas.onmouseup = function() {
+            canvas.onmousemove = null;
+            canvas.onmouseup = null;
+            canvas.style.cursor = "default";
+          };
+        } else if (event.button == 0) {
+          //鼠标点击事件
+          const imgX = _this.imgX;
+          const imgY = _this.imgY;
+          const image_width = _this.img.width;
+          const image_height = _this.img.height;
+          const imgScale = _this.imgScale;
+          //实际点的坐标
+          var isX = 0;
+          var isY = 0;
+          //实际选中坐标 肯定 = 点击的坐标 + canvas的坐标(必须为-数)
+          var temp_canvas_x = imgX - imgX * 2;
+          var temp_canvas_y = imgY - imgY * 2;
+
+          if (imgScale == 1) {
+            //原始尺寸
+            isX = pos.x + temp_canvas_x;
+            isY = pos.y + temp_canvas_y;
+          } else {
+            if (imgScale > 1) {
+              //被放大了,实际坐标需要/
+              isX = (pos.x + temp_canvas_x) / imgScale;
+              isY = (pos.y + temp_canvas_y) / imgScale;
+            } else {
+              //被缩小了,坐标点放大
+              isX = (pos.x + temp_canvas_x) * (1 / imgScale);
+              isY = (pos.y + temp_canvas_y) * (1 / imgScale);
+            }
+          }
+
+          //处理左上的是否超出图片范围
+          if (isX < 0 || isY < 0) {
+            _this.$message.error("请选择图片区域");
+            return false;
+          }
+          //处理右下区域,主要处理图片是否超出
+          if (isX > image_width || isY > image_height) {
+            _this.$message.error("超出图片尺度");
+            return false;
+          }
+          _this.dotX = isX;
+          _this.dotY = isY;
+          console.log("点击相对于图片的坐标点: x=" + isX + " & y=" + isY);
+          console.log(
+            "图片完整的宽度=" + image_width + "&高度=" + image_height
+          );
+          console.log("缩放级别:" + imgScale);
+        }
       };
 
       canvas.onmousewheel = canvas.onwheel = function() {
@@ -113,56 +171,6 @@ export default {
         _this.drawImage(); //重新绘制图片
         event.preventDefault && event.preventDefault(); // 阻止默认事件，可能在滚动的时候，浏览器窗口也会滚动
         return false;
-      };
-
-      canvas.onclick = function(evt) {
-        evt.stopPropagation();
-        evt.preventDefault();
-
-        const pos = _this.windowToCanvas(evt.clientX, evt.clientY);
-        const imgX = _this.imgX;
-        const imgY = _this.imgY;
-        const image_width = _this.img.width;
-        const image_height = _this.img.height;
-        const imgScale = _this.imgScale;
-        //实际点的坐标
-        var isX = 0;
-        var isY = 0;
-        //实际选中坐标 肯定 = 点击的坐标 + canvas的坐标(必须为-数)
-        var temp_canvas_x = imgX - imgX * 2;
-        var temp_canvas_y = imgY - imgY * 2;
-
-        if (imgScale == 1) {
-          //原始尺寸
-          isX = pos.x + temp_canvas_x;
-          isY = pos.y + temp_canvas_y;
-        } else {
-          if (imgScale > 1) {
-            //被放大了,实际坐标需要/
-            isX = (pos.x + temp_canvas_x) / imgScale;
-            isY = (pos.y + temp_canvas_y) / imgScale;
-          } else {
-            //被缩小了,坐标点放大
-            isX = (pos.x + temp_canvas_x) * (1 / imgScale);
-            isY = (pos.y + temp_canvas_y) * (1 / imgScale);
-          }
-        }
-
-        //处理左上的是否超出图片范围
-        if (isX < 0 || isY < 0) {
-          _this.$message.error("请选择图片区域");
-          return false;
-        }
-        //处理右下区域,主要处理图片是否超出
-        if (isX > image_width || isY > image_height) {
-          _this.$message.error("超出图片尺度");
-          return false;
-        }
-        _this.dotX = isX;
-        _this.dotY = isY;
-        console.log("点击相对于图片的坐标点: x=" + isX + " & y=" + isY);
-        console.log("图片完整的宽度=" + image_width + "&高度=" + image_height);
-        console.log("缩放级别:" + imgScale);
       };
     },
     windowToCanvas(x, y) {
